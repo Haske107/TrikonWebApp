a146cb9c4ed08e713174e88bb264866b8627488c
Haske107 <haske107@mail.chapman.edu>
2019-10-11 01:27:24 -0700

    5 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatCardModule,
+  imports:      [ BrowserModule, FormsModule, MatCardModule, MatGridListModule,
   
   RouterModule.forRoot([
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
   ]) ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent],
+  declarations: [ AppComponent, HomeComponent, AboutComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  //Projects
+  Acapella = "1jgddUwNk72BuXGTVT8uXdvH0aw0e9cA0";
+  AcapellaTitled = "1Cn6hX8GvrawGXZoqzJZ7CJ1ZGpGHv5zL";
+
+  AcapellaZ1 = "1vCY9JfLrakdgGPvs2LruiU_rqv98SVVv";
+  AcapelaZ1Titled = "1a3sMWz1dInEH3H-KgQ3SExusq3TImEsd";
+
+  BigMan = "1goaEkNSESX8fZcJ3KqhSXTYLq1pz5sIb";
+  BigManTitled = "1TfdHCcJ1PQ0wSLS9p5dtQqw1LQar_xe6";
+
+  Bwof = "1dg3o0CL_EmoOzPhC8aMbFLwFcSxzEVHk";
+  BwofTitled = "1Qg8BN2tuQTKmOa252jML_0SyGvTQ3gmV";
+
+  BlackParty = "1jK5CWD_Dn580fChGVk32LxrQRJu-aA2t";
 
   BannerLink = "https://player.vimeo.com/video/363450178?title=0&byline=0&portrait=0&loop=1&player_id=iframe59217?autoplay=1&autopause=0&muted=1&background=1&playsinline=1";
+
+  GoogleBaseLink = "https://drive.google.com/uc?export=view&id="
+  ProjectIDs = [
+    this.Acapella, this.AcapellaZ1, this.BigMan, this.Bwof, this.BlackParty
+  ];
+
+
+
+
 
   constructor() { }
 
